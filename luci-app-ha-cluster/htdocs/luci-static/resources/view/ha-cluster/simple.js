a8bd5f6d4d402823c6e97548fe35c056a0de303b
Pierre Gaufillet <pierre.gaufillet@bergamote.eu>
2026-03-09 16:31:17 +0100

    Simple page redesign: auto-manage VRRP instances
    Hide VRRP instance complexity from users in the simple page.
    Instances are auto-created per interface with deterministic VRID
    (DJB2 hash), garbage-collected when last VIP is deleted.
    
    New UI elements: vrrp_transport toggle (multicast/unicast),
    sync_enabled per-peer checkbox, read-only VRID column in VIP table.

--- luci-app-ha-cluster/htdocs/luci-static/resources/view/ha-cluster/simple.js
+++ luci-app-ha-cluster/htdocs/luci-static/resources/view/ha-cluster/simple.js
@@ -64,6 +64,14 @@ return view.extend({
 			return addr && addr.indexOf(':') !== -1;
 		};
 
+		// Deterministic VRID from interface name (DJB2 hash, range 1-127)
+		var computeVrid = function(ifname) {
+			var hash = 5381;
+			for (var i = 0; i < ifname.length; i++)
+				hash = ((hash << 5) + hash + ifname.charCodeAt(i)) & 0xffffffff;
+			return ((hash >>> 0) % 127) + 1;
+		};
+
 		var ifaceIconUrl = function(ifname) {
 			var dev = null;
 			for (var i = 0; i < netDevs.length; i++) {
@@ -93,6 +101,13 @@ return view.extend({
 		o.datatype = 'range(1,255)';
 		o.placeholder = '100';
 		o.default = '100';
+		o.rmempty = false;
+
+		o = s.option(form.ListValue, 'vrrp_transport', _('VRRP Transport'),
+			_('Multicast is the default. Use unicast when multicast is unavailable on the network.'));
+		o.value('multicast', _('Multicast'));
+		o.value('unicast', _('Unicast'));
+		o.default = 'multicast';
 		o.rmempty = false;
 
 		// Peer Configuration
@@ -144,6 +159,10 @@ return view.extend({
 				});
 			});
 
+			var peerSyncEnabled = s2.option(form.Flag, 'sync_enabled', _('Enable Synchronization'),
+				_('When disabled, this peer participates in VRRP failover but is excluded from config and lease sync. Use for non-OpenWrt peers.'));
+			peerSyncEnabled.default = '1';
+
 			m2.render().then(function(nodes) {
 				ui.showModal(_('Add Peer Router'), [
 					nodes,
@@ -158,6 +177,7 @@ return view.extend({
 								var nameVal = peerName.formvalue('_new_');
 								var addrVal = peerAddress.formvalue('_new_');
 								var sourceVal = peerSource.formvalue('_new_');
+								var syncVal = peerSyncEnabled.formvalue('_new_');
 
 								if (!nameVal || !addrVal) {
 									ui.addNotification(null, E('p', _('Please fill in all fields')), 'warning');
@@ -186,6 +206,7 @@ return view.extend({
 									uci.set('ha-cluster', newSection, 'name', nameVal);
 									uci.set('ha-cluster', newSection, 'address', addrVal);
 									uci.set('ha-cluster', newSection, 'source_address', sourceVal);
+									uci.set('ha-cluster', newSection, 'sync_enabled', syncVal || '1');
 								}).then(function() {
 									ui.hideModal();
 									return mainMap.load();
@@ -265,14 +286,40 @@ return view.extend({
 			return true;
 		};
 
+		o = s.option(form.Flag, 'sync_enabled', _('Enable Synchronization'),
+			_('When disabled, this peer participates in VRRP failover but is excluded from config and lease sync. Use for non-OpenWrt peers.'));
+		o.default = '1';
+		o.rmempty = false;
+		o.modalonly = true;
 
 		// Virtual IP Configuration
 		s = m.section(form.GridSection, 'vip', _('Virtual IP Addresses'),
-			_('Configure VIPs for each interface.'));
-		s.anonymous = false;
+			_('Configure virtual IP addresses. VIPs on the same interface fail over together.'));
+		s.anonymous = true;
 		s.addremove = true;
 		s.sortable = true;
 		s.addbtntitle = _('Add VIP...');
+
+		// Garbage collection: remove orphaned vrrp_instance when last VIP is deleted
+		s.handleRemove = function(section_id, ev) {
+			var instName = uci.get('ha-cluster', section_id, 'vrrp_instance') || '';
+
+			uci.remove('ha-cluster', section_id);
+
+			if (instName) {
+				var hasVips = uci.sections('ha-cluster', 'vip').some(function(vip) {
+					return vip.vrrp_instance === instName;
+				});
+				if (!hasVips)
+					uci.remove('ha-cluster', instName);
+			}
+
+			return this.map.save(null, true).catch(function(err) {
+				ui.addNotification(null,
+					E('p', {}, _('Failed to save: ') + err.message),
+					'danger');
+			});
+		};
 
 		o = s.option(form.DummyValue, '_interface_display', _('Interface'));
 		o.textvalue = function(section_id) {
@@ -284,10 +331,17 @@ return view.extend({
 		};
 		o.modalonly = false;
 
+		o = s.option(form.DummyValue, '_vrid_display', _('VRID'));
+		o.textvalue = function(section_id) {
+			var inst = uci.get('ha-cluster', section_id, 'vrrp_instance') || '';
+			if (!inst) return '-';
+			return uci.get('ha-cluster', inst, 'vrid') || '-';
+		};
+		o.modalonly = false;
+
 		o = s.option(form.ListValue, 'interface', _('Interface'));
 		o.rmempty = false;
 		o.modalonly = true;
-		o.editable = true;
 		// Populate with available interfaces
 		if (interfaces && interfaces.length) {
 			var seen = {};
@@ -307,6 +361,35 @@ return view.extend({
 				this.value(ifname, ifname);
 			}
 			return ifname;
+		};
+		o.write = function(section_id, value) {
+			var oldInst = uci.get('ha-cluster', section_id, 'vrrp_instance');
+
+			uci.set('ha-cluster', section_id, 'interface', value);
+
+			// Auto-create or attach vrrp_instance for this interface
+			var instName = value + '_1';
+			var exists = uci.sections('ha-cluster', 'vrrp_instance').some(function(inst) {
+				return inst['.name'] === instName;
+			});
+			if (!exists) {
+				var globalPriority = uci.get('ha-cluster', 'config', 'node_priority') || '100';
+				uci.add('ha-cluster', 'vrrp_instance', instName);
+				uci.set('ha-cluster', instName, 'vrid', String(computeVrid(value)));
+				uci.set('ha-cluster', instName, 'interface', value);
+				uci.set('ha-cluster', instName, 'priority', globalPriority);
+				uci.set('ha-cluster', instName, 'nopreempt', '1');
+			}
+			uci.set('ha-cluster', section_id, 'vrrp_instance', instName);
+
+			// Garbage collect old instance if interface changed
+			if (oldInst && oldInst !== instName) {
+				var hasVips = uci.sections('ha-cluster', 'vip').some(function(vip) {
+					return vip['.name'] !== section_id && vip.vrrp_instance === oldInst;
+				});
+				if (!hasVips)
+					uci.remove('ha-cluster', oldInst);
+			}
 		};
 
 		o = s.option(form.Value, 'address', _('Virtual IP Address (IPv4)'));
@@ -335,7 +418,7 @@ return view.extend({
 		o.placeholder = 'fd00::1';
 		o.rmempty = true;
 		o.modalonly = true;
-		o.description = _('Optional. If both IPv4 and IPv6 are configured, keepalived will create two separate VRRP instances (IPv4 uses VRID, IPv6 uses VRID+1).');
+		o.description = _('Optional. If IPv6 is configured, keepalived creates a second instance with VRID+128.');
 		o.validate = function(section_id, value) {
 			var addr = this.map.lookupOption('address', section_id);
 			var addrval = addr ? addr[0].formvalue(section_id) : '';
@@ -356,51 +439,6 @@ return view.extend({
 		o.default = '1';
 		o.rmempty = false;
 		o.modalonly = true;
-
-		o = s.option(form.Value, 'vrid', _('VRRP Router ID'),
-			_('Virtual Router ID (1-255). Must be unique per VIP and same on both routers. If IPv6 is also configured, IPv6 will use VRID+1.'));
-		o.datatype = 'range(1,255)';
-		o.placeholder = '51';
-		o.rmempty = false;
-		o.modalonly = true;
-		o.validate = function(section_id, value) {
-			if (!value)
-				return true;
-			var vrid = parseInt(value);
-			if (vrid < 1 || vrid > 255)
-				return _('VRID must be between 1 and 255');
-
-			// Check if IPv6 is configured
-			var addr6 = this.map.lookupOption('address6', section_id);
-			var addr6val = addr6 ? addr6[0].formvalue(section_id) : '';
-
-			if (addr6val && vrid === 255)
-				return _('VRID cannot be 255 when IPv6 is configured (IPv6 would use VRID 256, which is out of range)');
-
-			// Check for VRID uniqueness on the same interface
-			var iface_opt = this.map.lookupOption('interface', section_id);
-			var my_iface = iface_opt ? iface_opt[0].formvalue(section_id) : '';
-			var sections = uci.sections('ha-cluster', 'vip');
-			for (var i = 0; i < sections.length; i++) {
-				if (sections[i]['.name'] === section_id)
-					continue;
-				var other_iface = uci.get('ha-cluster', sections[i]['.name'], 'interface');
-				if (other_iface !== my_iface)
-					continue;
-				var other_vrid = parseInt(uci.get('ha-cluster', sections[i]['.name'], 'vrid'));
-				if (other_vrid === vrid)
-					return _('VRID %d is already used by VIP "%s" on the same interface').format(vrid, sections[i]['.name']);
-				// Also check IPv6 VRID+1 collisions on same interface
-				var other_addr6 = uci.get('ha-cluster', sections[i]['.name'], 'address6');
-				if (other_addr6 && other_vrid + 1 === vrid)
-					return _('VRID %d collides with IPv6 VRID (VRID+1) of VIP "%s"').format(vrid, sections[i]['.name']);
-				if (addr6val && vrid + 1 === other_vrid)
-					return _('IPv6 VRID %d (VRID+1) collides with VIP "%s"').format(vrid + 1, sections[i]['.name']);
-			}
-
-			return true;
-		};
-
 
 		// Configuration Synchronization (files + method)
 		s = m.section(form.NamedSection, 'config', 'global', _('Configuration Synchronization'),
